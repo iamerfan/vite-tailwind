cb36f26b3afdabdb3986c5b5655dd44d6366d73c
Erfan <erfanhosseinizadeh@gmail.com>
2023-11-27 14:27:46 +0330

    fix router

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,3 @@
 export default function App() {
-  return <h1>Vite + React</h1>;
+  return <h1 className="text-2xl font-bold">Vite + React</h1>;
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
 import "./index.css";
+
+const Routes = [
+  { path: "/", element: <App /> },
+  { path: "/about", element: <>About</> },
+];
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={createBrowserRouter(Routes)} />
   </React.StrictMode>,
 );
